b1e56cfa4eadd247ff81e197f33d3aaaa4d928dd
Arne Coucheron <arco68@gmail.com>
2013-08-21 07:10:36 +0200

    Squashed update of kernel from 3.4.58 to 3.4.59
     Conflicts:
    	drivers/hid/hid-microsoft.c

--- kernel/futex.c
+++ kernel/futex.c
@@ -61,6 +61,7 @@
 #include <linux/nsproxy.h>
 #include <linux/ptrace.h>
 #include <linux/bootmem.h>
+#include <linux/hugetlb.h>
 #include <asm/futex.h>
 
 #include "rtmutex_common.h"
@@ -481,7 +482,7 @@ again:
 	} else {
 		key->both.offset |= FUT_OFF_INODE; /* inode-based key */
 		key->shared.inode = page_head->mapping->host;
-		key->shared.pgoff = page_head->index;
+		key->shared.pgoff = basepage_index(page);
 	}
 
 	get_futex_key_refs(key);
